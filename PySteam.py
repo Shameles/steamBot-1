eef5afa1d265fc2f15d8962d85ad52c9e147d6b3
Thomas Wagenaar <thomaswagenaar99@gmail.com>
2015-07-18 19:06:31 +0200

    Summon & Command the bot
    You can now summon the bot and then give it orders. More orders are
    being coded, to check out future orders take a look at the TODO folder!
    Next up: create a buy order!

--- PySteam.py
+++ PySteam.py
@@ -7,151 +7,102 @@ import webbrowser
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 
+# Construct and install the HTTP(cookiejar)-Opener
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+urllib.request.install_opener(opener)
 
 class steamBot():
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
-        print('[X] Requesting Session ID')
-
+    
         url = 'http://steamcommunity.com/'
         values = {}
+        post = urllib.parse.urlencode(values)
+        headers = {}
+        binary_data = post.encode('utf-8')
+        request = urllib.request.Request(url, binary_data, headers)
+        resp = urllib.request.urlopen(request)
+
+        for cookie in cj:
+            print(cookie.name, cookie.value)
+            if cookie.name == 'sessionid':
+                self.sessionid = cookie.value
+            if cookie.name == 'steamCountry':
+                self.steamcountry = cookie.value
+
+    def getRSA(self):
+
+        url = 'https://steamcommunity.com/login/getrsakey/'
+        values = {'username' : 'icraftedyou', 'donotcache' : str(int(time.time()*1000))}
         post = urllib.parse.urlencode(values)
         binary_data = post.encode('utf-8')
         headers = {}
         request = urllib.request.Request(url, binary_data, headers)
-        response = urllib.request.urlopen(request)
-
-        # Process the data
-        headers = str(response.info())
-
-        SESSION_ID=headers[headers.find('sessionid') + 10:headers.find('sessionid') + 34]
-        print('- Session ID: ' + SESSION_ID)
-        
-        STEAM_COUNTRY=headers[headers.find('steamCountry') + 13:headers.find('steamCountry') + 50]
-        print('- Country: ' + STEAM_COUNTRY[0:2])
-
-        # Request an RSA key
-
-        print('[X] Requesting RSA Key')
-        
-        url = 'https://steamcommunity.com/login/getrsakey/'
-        values = {'username' : username, 'donotcache' : str(int(time.time()*1000))}
-        post = urllib.parse.urlencode(values)
-        binary_data = post.encode('utf-8')
-        headers = {'Cookie' : 'sessionid=' + SESSION_ID + '; ' + 'steamCountry=' + STEAM_COUNTRY + '; ' + 'timezoneOffset=7200,0;'}
-        request = urllib.request.Request(url, binary_data, headers)
         response = urllib.request.urlopen(request).read()
 
-        # Process the data
         data = json.loads(response.decode('utf-8'))
 
+        self.rsatimestamp = data['timestamp']
         mod = int(str(data['publickey_mod']), 16)
-        print('- MOD: ' + str(mod)[0:11] + '...')
-        
+
         exp = int(str(data['publickey_exp']), 16)
-        print('- EXP: ' + str(exp))
+
+        steamid = data['steamid']
 
         rsa_key = RSA.construct((mod, exp))
         rsa = PKCS1_v1_5.new(rsa_key)
-        encrypted_password = rsa.encrypt(password.encode('utf-8'))
-        encrypted_password = base64.b64encode(encrypted_password)
+        self.encrypted_password = rsa.encrypt('Kat12345'.encode('utf-8'))
+        self.encrypted_password = base64.b64encode(self.encrypted_password)
 
-        # Do the login
-
-        print('[X] Attempting login')
-
+    def doLogin(self, type):
         url = 'https://steamcommunity.com/login/dologin'
-        values = {'password' : encrypted_password.decode('utf-8'),
-                  'username' : username,
+        values = {'password' : self.encrypted_password.decode('utf-8'),
+                  'username' : self.username,
                   'twofactorcode' : '',
-                  'emailauth' : '',
+                  'emailauth' : '' if type != 'emailauth' else str(input('Please input the key you received by e-mail: ')),
                   'loginfriendlyname' : '',
-                  'captchagid' : '-1',
-                  'captcha_text' : '',
+                  'captchagid' : '-1' if type != 'captcha' else self.captchagid,
+                  'captcha_text' : '' if type != 'captcha' else str(input('Please input the captcha')),
                   'emailsteamid' : '',
-                  'rsatimestamp' : str(data['timestamp']),
+                  'rsatimestamp' : self.rsatimestamp,
                   'remember_login' : 'false',
                   'donotcache' : str(int(time.time()*1000))}
-        headers = {'Cookie' : 'sessionid=' + SESSION_ID + '; ' + 'steamCountry=' + STEAM_COUNTRY + '; ' + 'timezoneOffset=7200,0;'}
+        headers = {}
         post = urllib.parse.urlencode(values)
         binary_data = post.encode('utf-8')
         request = urllib.request.Request(url, binary_data, headers)
         response = urllib.request.urlopen(request).read()
 
-        # Process the data
         data = json.loads(response.decode('utf-8'))
-
-        if data['emailauth_needed'] == True:
-            print('- Result: Email-authentication needed')
-            print('- E-mail: ...' + data['emaildomain'])
-
-        EMAIL_STEAM_ID = str(data['emailsteamid'])
-
-        # Here we have to solve the e-mail auth
-
-        # Request an RSA key again
-
-        print('[X] Requesting RSA Key')
-
-        url = 'https://steamcommunity.com/login/getrsakey/'
-        values = {'username' : username, 'donotcache' : str(int(time.time()*1000))}
-        post = urllib.parse.urlencode(values)
-        binary_data = post.encode('utf-8')
-        headers = {'Cookie' : 'sessionid=' + SESSION_ID + '; ' + 'steamCountry=' + STEAM_COUNTRY + '; ' + 'timezoneOffset=7200,0;'}
-        request = urllib.request.Request(url, binary_data, headers)
-        response = urllib.request.urlopen(request).read()
-
-        # Process the data
-        data = json.loads(response.decode('utf-8'))
-
-        mod = int(str(data['publickey_mod']), 16)
-        print('- MOD: ' + str(mod)[0:11] + '...')
         
-        exp = int(str(data['publickey_exp']), 16)
-        print('- EXP: ' + str(exp))
-
-        rsa_key = RSA.construct((mod, exp))
-        rsa = PKCS1_v1_5.new(rsa_key)
-        encrypted_password = rsa.encrypt(password.encode('utf-8'))
-        encrypted_password = base64.b64encode(encrypted_password)
-                  
-        # Do the login again
-
-        print('[X] Attempting login')
-
-        url = 'https://steamcommunity.com/login/dologin'
-        values = {'password' : encrypted_password.decode('utf-8'),
-                  'username' : username,
-                  'twofactorcode' : '',
-                  'emailauth' : str(input('Please input the key you received by e-mail: ')),
-                  'loginfriendlyname' : '',
-                  'captchagid' : '-1',
-                  'captcha_text' : '',
-                  'emailsteamid' : EMAIL_STEAM_ID,
-                  'rsatimestamp' : str(data['timestamp']),
-                  'remember_login' : 'false',
-                  'donotcache' : str(int(time.time()*1000))}
-        headers = {'Cookie' : 'sessionid=' + SESSION_ID + '; ' + 'steamCountry=' + STEAM_COUNTRY + '; ' + 'timezoneOffset=7200,0;'}
-        post = urllib.parse.urlencode(values)
-        binary_data = post.encode('utf-8')
-        request = urllib.request.Request(url, binary_data, headers)
-        response = urllib.request.urlopen(request).read()
-
-        #Process the data
-
-        data = json.loads(response.decode('utf-8'))
-
         if data['success'] == True:
-            print('[X] Login succesful!')
-            print('- Secure token: ' + data['transfer_parameters']['token_secure'][0:11] + '...')
-            print('- Token: ' + data['transfer_parameters']['token'][0:11] + '...')
-            print('- Webcookie: ' + data['transfer_parameters']['webcookie'][0:11] + '...')
-            print('- Auth: ' + data['transfer_parameters']['auth'][0:11] + '...')
-        else:
-            print('[X] Login failed!')
-            
-            
+            self.securetoken = data['transfer_parameters']['token_secure']
+            print(self.securetoken)
+            self.token = data['transfer_parameters']['token']
+            print(self.token)
+            self.webcookie = data['transfer_parameters']['webcookie']
+            print(self.webcookie)
+            self.auth = data['transfer_parameters']['auth']
+            print(self.auth)
 
-steamBot('yourusername', 'yourpassword')
+            for cookie in cj:
+                print(cookie.name, cookie.value)
+            return True
+        else:
+            if data['emailauth_needed'] == True:
+                print('- Result: Email-authentication needed')
+                print('- E-mail: ...' + data['emaildomain'])
+                self.emailsteamid = str(data['emailsteamid'])
+                self.emaildomain = data['emaildomain']
+
+            else:
+                print('[X] Login details incorrect')
+                return False
+
+# Command the bot here
+mybot = steamBot('username', 'password')
+mybot.getRSA()
+mybot.doLogin('')
+mybot.doLogin('emailauth')
